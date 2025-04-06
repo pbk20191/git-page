ad988bc6b7242acba908f442d491db15dc1813fb
Byeong Gwan <impbk2002@gmail.com>
2025-04-06 17:21:15 +0900

    second commit

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,17 @@
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
+
+/** @type {import('@sveltejs/kit').Config} */
 const config = {
 	preprocess: vitePreprocess(),
 	kit: {
-		adapter: adapter(),
+		adapter: adapter({
+			fallback:"404.html",
+
+		}),
 		paths: {
-			base: process.env.NODE_ENV === 'production' ? '/git-page' : '',
+			base: process.env.NODE_ENV === 'production' ? process.env.BASE_PATH : '',
 		}
 	}
 };
