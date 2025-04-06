e87390636ce6c7a8e5a3127785d39eba2ed2388d
Byeong Gwan <impbk2002@gmail.com>
2025-04-06 17:31:16 +0900

    fix static route path

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -2,6 +2,8 @@
 	import { page } from '$app/state';
 	import logo from '$lib/images/svelte-logo.svg';
 	import github from '$lib/images/github.svg';
+	import { base } from '$app/paths';
+
 </script>
 
 <header>
@@ -16,11 +18,11 @@
 			<path d="M0,0 L1,2 C1.5,3 1.5,3 2,3 L2,0 Z" />
 		</svg>
 		<ul>
-			<li aria-current={page.url.pathname === '/' ? 'page' : undefined}>
-				<a href="/">Home</a>
+			<li aria-current={page.url.pathname === base + "/" ? "page" : undefined}>
+				<a href="{base}/">Home</a>
 			</li>
-			<li aria-current={page.url.pathname === '/about' ? 'page' : undefined}>
-				<a href="/about">About</a>
+			<li aria-current={page.url.pathname === base + "/about" ? "page" : undefined}>
+				<a href="{base}/about">About</a>
 			</li>
 		</ul>
 		<svg viewBox="0 0 2 3" aria-hidden="true">
